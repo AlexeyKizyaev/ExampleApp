7da509ac66f2b839a99cf0ba0301ea7b3e6385b5
a.kizyaev <a.kizyaev@evotor.ru>
2024-12-17 17:00:13 +0300

    Сделал рабочий запрос к БД терминала

--- app/src/main/java/com/example/exampleapp/MainActivity.java
+++ app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.exampleapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,20 +23,37 @@ public class MainActivity extends IntegrationActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        var products = new ProductQuery()
-                .alcoholByVolume.between(new BigDecimal(40), new BigDecimal(70))
-                .and(new ProductQuery()
-                        .price.greater(new BigDecimal(100), true)
-                        .or().tareVolume.greater(new BigDecimal(10), true)
-                        .or(new ProductQuery()
-                                .price.lower(new BigDecimal(5), true)
-                                .and().name.like("Спиртное", null)
-                        )
-                ).sortOrder(new ProductQuery.SortOrder()
-                        .price.asc()
-                        .alcoholByVolume.desc()
-                        .tareVolume.asc()
-                ).limit(1000)
+        findViewById(R.id.button_getProduct).setOnClickListener(view -> getProduct());
+    }
+
+    public void getProduct() {
+        var products = new ProductQuery().price.lower(new BigDecimal(500), true)
+                .and().name.like("TestQR", null).limit(1000)
                 .execute(getApplicationContext());
+
+        if (products != null && products.moveToFirst()) {
+            StringBuilder productInfo = new StringBuilder();
+            do {
+                // Получение данных из курсора
+                String name = products.getString(products.getColumnIndex("NAME")); // колонка "NAME"
+                BigDecimal price = new BigDecimal(products.getString(products.getColumnIndex("PRICE_OUT"))); // колонка "PRICE_OUT"
+
+
+                // Формирование строки
+                productInfo.append("Название: ").append(name)
+                        .append(", Цена: ").append(price)
+                        .append("\n");
+            } while (products.moveToNext());
+
+            // Отображение данных
+            Toast.makeText(getApplicationContext(), productInfo.toString(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Продукты не найдены", Toast.LENGTH_SHORT).show();
+        }
+
+        // Закрываем Cursor для освобождения ресурсов
+        if (products != null) {
+            products.close();
+        }
     }
 }
